9b93f800a1bae037e47d2ae8157f22063393c47c
James Pakis <72415666+javapak@users.noreply.github.com>
2025-09-26 02:39:53 -0500

    Update SDFTextAtlas.ts

--- src/renderers/SDFTextAtlas.ts
+++ src/renderers/SDFTextAtlas.ts
@@ -42,7 +42,7 @@ export class SDFTextAtlas {
     this.sdf = new TinySDF({
       fontSize,
       buffer: this.buffer,
-      radius: 1.25,
+      radius: 1,
       cutoff: 0.1,
       fontFamily: 'system-ui, -apple-system, sans-serif',  
       fontStyle: 'normal',    

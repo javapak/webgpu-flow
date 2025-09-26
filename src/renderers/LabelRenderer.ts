ab17a93a61a50b91fcd86348b171fddae0ab82e5
Alex Pakis <alex.pakis@techngs.com>
2025-09-26 01:07:07 -0500

    soft labels

--- src/renderers/LabelRenderer.ts
+++ src/renderers/LabelRenderer.ts
@@ -219,7 +219,7 @@ export class LabelRenderer {
         if (layoutedGlyphs.length === 0) continue; 
         const nodeSize = Math.max(node.visual?.size?.width || 100, node.visual?.size?.height || 100);
         const referenceSize = 100; 
-        const scale = Math.sqrt(nodeSize / referenceSize) * 0.25; 
+        const scale = Math.sqrt(nodeSize / referenceSize) * 0.1; 
         let baseX = node.data.position.x;
         const baseY = node.data.position.y;
         

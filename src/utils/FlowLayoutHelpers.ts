1ca124abff0adeef162b51e84ece1fc522fc68a1
James Pakis <japakis@memphis.edu>
2026-06-09 10:37:39 -0500

    test

--- src/utils/FlowLayoutHelpers.ts
+++ src/utils/FlowLayoutHelpers.ts
@@ -1,4 +1,5 @@
-import ELK, { type ElkExtendedEdge } from 'elkjs/lib/elk.bundled.js';
+import ELK from 'elkjs';
+import  { type ElkExtendedEdge } from 'elkjs/lib/elk.bundled.js';
 
 const elk = new ELK();
 const GRID = 50;

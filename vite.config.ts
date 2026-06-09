5604469c9727666718da85b01dfb0c9ce5c4273a
James Pakis <japakis@memphis.edu>
2026-06-09 10:46:08 -0500

    plz elkjs stop it

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      'elkjs/lib/elk.bundled.js': 'elkjs/lib/elk.bundled.js',
-    }
-  },
+  plugins: [react()],
   optimizeDeps: {
-    exclude: ['elkjs/lib/elk.bundled.js'],
-    include: ['elkjs'],
+    // Force Vite to pre-bundle the base package into a clean ES Module
+    include: ['elkjs'], 
   },
-});
-
+})

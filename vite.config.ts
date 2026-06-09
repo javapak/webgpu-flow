2131e97b1cf149b02dad7efb2c5d7715259ab95e
James Pakis <japakis@memphis.edu>
2026-06-09 10:03:46 -0500

    fix path on import for gpuInstnace in DiagramProvider

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+  
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'elkjs/lib/elk.bundled.js': 'elkjs/lib/elk.bundled.js',
+    }
+  },
+  optimizeDeps: {
+    exclude: ['elkjs/lib/elk.bundled.js'],
+    include: ['elkjs'],
+  }
+});

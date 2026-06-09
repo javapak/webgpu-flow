b12c4a08cdad6a1ec2535f5be71b228773892730
James Pakis <japakis@memphis.edu>
2026-06-09 10:21:52 -0500

    fix path on import for gpuInstnace in DiagramProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {DiagramDemo} from './App.tsx'
 import { MantineProvider } from '@mantine/core'
 import { DiagramProvider } from './components/DiagramProvider.tsx';
+import ERFlow from './components/ERFlow.tsx';
 
 window.console.log = () => {};
 
 createRoot(document.getElementById('root')!).render(
         <MantineProvider defaultColorScheme='dark'>
                 <DiagramProvider>
-                        <DiagramDemo />
+                        <ERFlow />
                 </DiagramProvider>
         </MantineProvider>
 )
